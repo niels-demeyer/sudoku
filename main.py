73a43aa313fb6015c927995948f6d56fc873bf6a
niels demeyer <niels.demeyer99@gmail.com>
2024-02-23 17:44:36 +0100

    code to create the sudoku grid

---
+++ main.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+class Sudoku_grid:
+    def __init__(self):
+        self.grid = np.zeros((9, 9), dtype=int)
+
+    def generate(self):
+        for i in range(9):
+            for j in range(9):
+                self.grid[i][j] = 0
+
+    def print_grid(self):
+        print(self.grid)
+
+    def assign_value(self, row, col, value):
+        self.grid[row, col] = value
+
+    def make_exercise(self):
+        pass
+
+
+# Example of a grid
+sudoku = Sudoku_grid()
+sudoku.generate()
+sudoku.print_grid()
+sudoku.
